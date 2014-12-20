405c6570aa3f1888f9b7c2f4398321b896ea0798
leo <luzhantao1@163.com>
2014-12-20 22:14:23 +0800

    init

---
+++ src/main.lua
@@ -0,0 +1,72 @@
+
+cc.FileUtils:getInstance():addSearchPath("src")
+cc.FileUtils:getInstance():addSearchPath("res")
+
+-- CC_USE_DEPRECATED_API = true
+require "cocos.init"
+
+-- cclog
+cclog = function(...)
+    print(string.format(...))
+end
+
+-- for CCLuaEngine traceback
+function __G__TRACKBACK__(msg)
+    cclog("----------------------------------------")
+    cclog("LUA ERROR: " .. tostring(msg) .. "\n")
+    cclog(debug.traceback())
+    cclog("----------------------------------------")
+    return msg
+end
+
+local function main()
+    collectgarbage("collect")
+    -- avoid memory leak
+    collectgarbage("setpause", 100)
+    collectgarbage("setstepmul", 5000)
+
+    -- initialize director
+    local director = cc.Director:getInstance()
+    local glview = director:getOpenGLView()
+    if nil == glview then
+        glview = cc.GLViewImpl:createWithRect("SLGDemo", cc.rect(0,0,960,640))
+        director:setOpenGLView(glview)
+    end
+
+    glview:setDesignResolutionSize(960, 640, cc.ResolutionPolicy.NO_BORDER)
+
+    --turn on display FPS
+    director:setDisplayStats(true)
+
+    --set FPS. the default value is 1.0/60 if you don't call this
+    director:setAnimationInterval(1.0 / 60)
+
+    local schedulerID = 0
+    --support debug
+    local targetPlatform = cc.Application:getInstance():getTargetPlatform()
+    if (cc.PLATFORM_OS_IPHONE == targetPlatform) or (cc.PLATFORM_OS_IPAD == targetPlatform) or
+       (cc.PLATFORM_OS_ANDROID == targetPlatform) or (cc.PLATFORM_OS_WINDOWS == targetPlatform) or
+       (cc.PLATFORM_OS_MAC == targetPlatform) then
+        cclog("result is ")
+        --require('debugger')()
+
+    end
+    
+    -- run
+    local sceneGame = cc.Scene:create()
+
+    sceneGame:addChild(require("mainLayer").new())
+
+    if cc.Director:getInstance():getRunningScene() then
+        cc.Director:getInstance():replaceScene(sceneGame)
+    else
+        cc.Director:getInstance():runWithScene(sceneGame)
+    end
+
+end
+
+
+local status, msg = xpcall(main, __G__TRACKBACK__)
+if not status then
+    error(msg)
+end
---
+++ src/mainLayer.lua
@@ -0,0 +1,9 @@
+local mainLayer = class("mainLayer", function()
+	return cc.LayerColor:create(cc.c4b(255, 255, 0, 100))
+end)
+
+function mainLayer:ctor()
+	
+end
+
+return mainLayer
